2451f1f20c7825538e78566d31f5c2c8d35787be
lcxfs1991 <lcxfs1991@gmail.com>
2017-12-04 11:28:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# steamer-team-alloyteam
+
+alloyteam steamerjs 团队配置
+
+[![NPM Version](https://img.shields.io/npm/v/steamer-team-alloyteam.svg?style=flat)](https://www.npmjs.com/package/steamer-team-alloyteam)
+[![Deps](https://david-dm.org/steamerjs/steamer-team-alloyteam.svg)](https://david-dm.org/steamerjs/steamer-team-alloyteam)
+
+## 配置
+
+```javascript
+NPM = npm
+TEAM = alloyteam
+```
+
+## 脚手架
+* [steamer-react](https://github.com/steamerjs/steamer-react)
+* [steamer-react-hy](http://git.code.oa.com/steamer/steamer-react-hy)
+* [steamer-vue](https://github.com/steamerjs/steamer-vue)
+* [steamer-simple](https://github.com/steamerjs/steamer-simple)
+* [steamer-react-component](https://github.com/steamerjs/steamer-react-component)
+* [steamer-vue-component](https://github.com/steamerjs/steamer-vue-component)
+* [steamer-simple-component](https://github.com/steamerjs/steamer-simple-component)
+* [steamer-logic-component](https://github.com/steamerjs/steamer-logic-component)
+
+## 命令行插件
+* [steamer-plugin-mock](https://github.com/steamerjs/steamer-plugin-mock)
---
+++ index.js
@@ -0,0 +1,51 @@
+let teamConfig = {
+    config: {
+        NPM: 'tnpm',
+        TEAM: 'alloyteam'
+    },
+    kits: [
+        {
+            'name': 'steamer-react',
+            'git': 'https://github.com/steamerjs/steamer-react.git'
+        },
+        {
+            'name': 'steamer-react-hy',
+            'git': 'http://git.code.oa.com/steamer/steamer-react-hy.git'
+        },
+        {
+            'name': 'steamer-vue',
+            'git': 'https://github.com/steamerjs/steamer-vue.git'
+        },
+        {
+            'name': 'steamer-simple',
+            'git': 'https://github.com/steamerjs/steamer-simple.git'
+        },
+        {
+            'name': 'steamer-react-component',
+            'git': 'https://github.com/steamerjs/steamer-react-component.git'
+        },
+        {
+            'name': 'steamer-vue-component',
+            'git': 'https://github.com/steamerjs/steamer-vue-component.git'
+        },
+        {
+            'name': 'steamer-simple-component',
+            'git': 'https://github.com/steamerjs/steamer-simple-component.git'
+        },
+        {
+            'name': 'steamer-logic-component',
+            'git': 'https://github.com/steamerjs/steamer-logic-component.git'
+        }
+    ],
+    plugins: [
+        'steamer-plugin-mock'
+    ],
+    beforeInstall: function() {
+
+    },
+    afterInstall: function() {
+        
+    }
+};
+
+module.exports = teamConfig;
